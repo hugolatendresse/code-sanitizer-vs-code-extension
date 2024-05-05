ddca7833d0e00de0a1f5c20d4eac60fcb8cd7fc0
Hugo Latendresse <hugolatendresse@gmail.com>
2024-05-04 20:27:51 -0700

    both copy and paste work with palette and key bindings

---
+++ anonymizer.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const sqlReservedWordsUpper = require('./reserved_words');
+const wordList = require('./word_list');
+
+class Anonymizer {
+    constructor(tokenMode = 'dictionary') {
+        this.mapping = {}; // Dictionary from original tokens to sanitized tokens
+        this.tokenMode = tokenMode; // 'random' for random strings, 'dictionary' for dictionary words
+        if (tokenMode === 'dictionary') {
+            this.wordList = wordList;
+            // TODO the above is not very elegant, would be better to read from json file
+            // let rawData = fs.readFileSync('word_list.json', 'utf8');
+            // this.wordList = JSON.parse(rawData);
+            this.shuffleArray(this.wordList);
+        }
+    }
+
+    generateRandomString(length = 8) {
+        // TODO this is not as rigourous as in python!! Need to define a word exactly
+        if (this.tokenMode === 'random') {
+            let letters = 'abcdefghijklmnopqrstuvwxyz';
+            let result = '';
+            for (let i = 0; i < length; i++) {
+                result += letters.charAt(Math.floor(Math.random() * letters.length));
+            }
+            return result;
+        } else if (this.tokenMode === 'dictionary') {
+            if (!this.wordList.length) {
+                throw new Error("The word list has been exhausted.");
+            }
+            return this.wordList.pop();
+        } else {
+            throw new Error(`Unexpected token mode: ${this.tokenMode}`);
+        }
+    }
+
+    anonymize(query) {
+        this.mapping = {};
+        const tokens = query.match(/\b\w+\b/g);
+        tokens.forEach(token => {
+            const upperToken = token.toUpperCase();
+            if (!sqlReservedWordsUpper.includes(upperToken)) {
+                if (!this.mapping.hasOwnProperty(token)) {
+                    this.mapping[token] = this.generateRandomString();
+                }
+                query = this.replaceInString(token, this.mapping[token], query);
+            }
+        });
+        return query;
+    }
+
+    unanonymize(query) {
+        Object.entries(this.mapping).forEach(([originalToken, sanitizedToken]) => {
+            query = this.replaceInString(sanitizedToken, originalToken, query);
+        });
+        return query;
+    }
+
+    replaceInString(token, replacement, string) {
+        const regex = new RegExp(`\\b${token.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')}\\b`, 'g');
+        return string.replace(regex, replacement);
+    }
+
+    shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+        }
+    }
+}
+
+module.exports = Anonymizer;
---
+++ extension.js
@@ -0,0 +1,59 @@
+const vscode = require('vscode');
+const Anonymizer = require('./anonymizer');
+
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+    let a = new Anonymizer();
+
+	console.log('Extension "code-anonymizer" is now active!');
+
+    let disposable = vscode.commands.registerCommand('code-anonymizer.anonymizeAndCopy', function () {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const selection = editor.selection;
+            const text = editor.document.getText(selection);
+            // let a = new Anonymizer();
+            let modifiedText = a.anonymize(text);
+            // const modifiedText = text.replace(/JOIN/g, 'NOTAJOINANYMORE!!!!!');
+
+            vscode.env.clipboard.writeText(modifiedText)
+                .then(() => {
+                    vscode.window.showInformationMessage('Text modified and copied!');
+                }, (err) => {
+                    vscode.window.showErrorMessage('Failed to copy text: ' + err);
+                });
+        }
+    });
+
+    context.subscriptions.push(disposable);
+
+    let disposableUnanonymizeAndPaste = vscode.commands.registerCommand('code-anonymizer.unanonymizeAndPaste', async function () {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // const Anonymizer = require('./anonymizer');
+           
+            let clipboardText = await vscode.env.clipboard.readText();
+            let modifiedText = a.unanonymize(clipboardText);
+    
+            editor.edit(editBuilder => {
+                editBuilder.insert(editor.selection.start, modifiedText);
+            });
+        }
+    });
+    
+    context.subscriptions.push(disposableUnanonymizeAndPaste);
+
+
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
