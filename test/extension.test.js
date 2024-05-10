4200afd0821e3dfd9067d4f34f4e39fe73593842
Hugo Latendresse <hugolatendresse@gmail.com>
2024-05-10 16:57:46 -0700

    all tests pass!

--- test/extension.test.js
+++ test/extension.test.js
@@ -21,6 +21,23 @@ function assertAllTokensDifferent(text1, text2) {
 		});
 }
 
+// The following function returns the tokens that are the same in the two texts, and asserts all other tokens are different
+function assertSomeTokensSame(text1, text2, sameExpectedTokens) {
+	assert.notStrictEqual(text1, text2);
+	const tokens1 = text1.match(/\b\w+\b/g);
+	const tokens2 = text2.match(/\b\w+\b/g);
+	const sameTokens = [];
+	tokens1.forEach(token => {
+		if (tokens2.includes(token)) {
+			sameTokens.push(token);
+		} else {
+			assert.strictEqual(tokens2.includes(token), false);
+		}
+	});
+	// Check that the common tokens are as expected, ignoring the order
+	assert.deepStrictEqual(Array.from(new Set(sameTokens)).sort(), Array.from(new Set(sameExpectedTokens)).sort());
+}
+
 suite('Extension Test Suite', () => {
 
 	test('Test 00 anonymizeAndCopy all sanitized', async () => {
@@ -149,7 +166,7 @@ suite('Extension Test Suite', () => {
 		await editor.edit(editBuilder => {
 			editBuilder.insert(new vscode.Position(0, 0), originalText);
 		});
-		printDebugInfo("originalText (should be full length and unsanitized", originalText);
+		// printDebugInfo("originalText (should be full length and unsanitized", originalText);
 
 		// Copy and sanitize
 		let lineCount = editor.document.lineCount;
@@ -200,19 +217,97 @@ suite('Extension Test Suite', () => {
 
 		// Replace all text in the editor with the unsanitized text
 		assertAllTokensDifferent(originalText, document.getText());
-
-		// Replace all text with unanonymized version
 		await vscode.commands.executeCommand('editor.action.selectAll');
 		await vscode.commands.executeCommand('code-sanitizer.unanonymizeAndPaste');
 
 		// Assert that the finalText is equal to the first 7 lines of the originalText
 		const originalTextHalf = originalText.split('\n').slice(0, halfLines).join('\n') + '\r\n';
-		printDebugInfo("originalTextHalf", originalTextHalf);
-		printDebugInfo("finalText", document.getText());
+		// printDebugInfo("originalTextHalf", originalTextHalf);
+		// printDebugInfo("finalText", document.getText());
+		assert.strictEqual(document.getText().replace(/\r\n/g, '\n'), originalTextHalf.replace(/\r\n/g, '\n'));
+    });
+
+    test('Test 04 unanonymizeAndPaste half, with SQL keywords', async () => {
+		let doc = await vscode.workspace.openTextDocument({ content: ' ' });
+		let editor = await vscode.window.showTextDocument(doc);
+		let document = editor.document;
+		assert.ok(editor, 'No active editor');
+		const originalText = `Wewilltry.amuchlonger pieceoftext tocheck ifthe unanonymize functionworksproperly
+		thisisjust averylong stringwithlotsofwords LEFT JOIN moreoverthere aresomenumbers1234
+		where specialcharacterslike $%&/()=?^* ORDER BY andalsosome! SELECT
+		andthen, itrepeasts twotimes
+		Wewilltry.amuchlonger pieceoftext tocheck ifthe unanonymize functionworksproperly
+		thisisjust averylong stringwithlotsofwords LEFT JOIN moreoverthere aresomenumbers1234
+		where specialcharacterslike $%&/()=?^* andalsosome SELECT
+		Wewilltry.amuchlonger pieceoftext tocheck ifthe unanonymize functionworksproperly
+		thisisjust averylong stringwithlotsofwords LEFT JOIN moreoverthere aresomenumbers1234
+		where specialcharacterslike $%&/()=?^* ORDER BY andalsosome! SELECT
+		andtheend`;
+		const sameExpectedTokens = ["SELECT", "ORDER", "BY", "where", "LEFT", "JOIN"];
+		await editor.edit(editBuilder => {
+			editBuilder.insert(new vscode.Position(0, 0), originalText);
+		});
+		printDebugInfo("originalText (should be full length and unsanitized", originalText);
+
+		// Copy and sanitize
+		let lineCount = editor.document.lineCount;
+		editor.selection = new vscode.Selection(0, 0, lineCount - 1, editor.document.lineAt(lineCount - 1).text.length);
+		await vscode.commands.executeCommand('code-sanitizer.anonymizeAndCopy');
+
+		// Clear the editor
+		await vscode.commands.executeCommand('editor.action.selectAll');
+		await vscode.commands.executeCommand('editor.action.deleteLines');
+
+		// Paste in editor so that editor contains sanitized text
+		await vscode.commands.executeCommand('editor.action.clipboardPasteAction');
+
+		// Assert that every token in selection is different from the original text, except for SQL words
+		assertSomeTokensSame(originalText, document.getText(), sameExpectedTokens);
+
+		// Assert that the third line of pastedText is the same as penultimate line of pastedText
+		// This is to make sure that same symbols get mapped to same sanitized tokens
+		const pastedLines = document.getText().split('\n');
+		assert.strictEqual(pastedLines[2], pastedLines[9]);
+
+		// Set clipboard to something random
+		await vscode.env.clipboard.writeText("random text");
+
+		// Copy half of text in the editor such that clipboard contains 7 lines of sanitized text
+		let halfLines = 7;
+		editor.selection = new vscode.Selection(0, 0, halfLines, 0);
+		await vscode.commands.executeCommand('editor.action.clipboardCopyAction');
+
+		// Assert that every token in selection is different from the original text, except for SQL words
+		assertSomeTokensSame(originalText, await vscode.env.clipboard.readText(), sameExpectedTokens);
+
+		// Replace all text in the editor with "hello world"
+		// const text_before_helloworld = document.getText();
+		// printDebugInfo("text_before_priting_hello_world", text_before_helloworld);
+		await vscode.commands.executeCommand('editor.action.selectAll');
+		await vscode.commands.executeCommand('editor.action.deleteLines');		
+		await editor.edit(editBuilder => {
+			// Create a range that covers the entire document
+			let range = new vscode.Range(
+				document.positionAt(0),
+				document.positionAt(document.getText().length)
+			);
+
+			// Replace the range with "hello world"
+			editBuilder.replace(range, "hello world");
+		});
+
+		// Replace all text in the editor with the unsanitized text
+		assertAllTokensDifferent(originalText, document.getText());  // Should have nothing in common with Hello World
+		await vscode.commands.executeCommand('editor.action.selectAll');
+		await vscode.commands.executeCommand('code-sanitizer.unanonymizeAndPaste');
+
+		// Assert that the finalText is equal to the first 7 lines of the originalText
+		const originalTextHalf = originalText.split('\n').slice(0, halfLines).join('\n') + '\r\n';
+		// printDebugInfo("originalTextHalf", originalTextHalf);
+		// printDebugInfo("finalText", document.getText());
 		assert.strictEqual(document.getText().replace(/\r\n/g, '\n'), originalTextHalf.replace(/\r\n/g, '\n'));
     });
 
 
-	// TODO    test('Test 05 unanonymizeAndPaste with SQL keywords', async () => {
 
 });
