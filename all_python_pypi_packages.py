fb90ddb3b94b561bdfee8b416ba0e43e2d335b68
Hugo Latendresse <hugolatendresse@gmail.com>
2024-05-12 06:59:52 -0700

    include standard lib

--- all_python_pypi_packages.py
+++ all_python_pypi_packages.py
@@ -3,12 +3,16 @@ import re
 import json
 # Load projects from a .json file
 projects = json.loads(open("top-pypi-packages-30-days.min.json").read())
-print("1")
 # Extract the project names in projects
 project_names = [project["project"] for project in projects]
-print("2")
+
+# Add standard library!
+import sys
+project_names.extend(list(sys.stdlib_module_names))
 
 # Save to json
 with open("top-pypi-project-names.json", "w") as f:
     json.dump(project_names, f)
-print("3")
+
+print('done')
+
