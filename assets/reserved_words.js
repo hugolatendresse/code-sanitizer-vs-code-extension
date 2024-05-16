ab2b1fd4a6f9770e6ea34e068f8a399beef94044
Hugo Latendresse <hugolatendresse@gmail.com>
2024-05-16 09:28:41 -0700

    negative numbers

--- assets/reserved_words.js
+++ assets/reserved_words.js
@@ -25,7 +25,8 @@ const sqlReservedWordsUpper = [
     "UNION", "UNIQUE", "UNPIVOT", "UPDATE", "UPDATETEXT", "USE", "USER", "VALUES",
     "VARYING", "VIEW", "WAITFOR", "WHEN", "WHERE", "WHILE", "WITH", "WITHIN GROUP",
     "WRITETEXT", "XOR", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
-    "12", "13", "14", "15", "16", "17", "18", "19", "20"
+    "12", "13", "14", "15", "16", "17", "18", "19", "20",
+    "-1", "-2", "-3", "-4", "-5", "-5", "-6", "-7", "-8", "-9", "-10", "-11",
 ];
 
 const pythonReservedWords = [
